80eafdd14e52bbf81d993f9992bc83654cc807ea
razvanaldea89 <razvan.aldea89@yahoo.ro>
2019-02-08 19:58:58 +0200

    fix potential bug with other plugins / version bump /  changelog update

--- simple-custom-post-order.php
+++ simple-custom-post-order.php
@@ -3,7 +3,7 @@
  *Plugin Name: Simple Custom Post Order
  *Plugin URI: https://wordpress.org/plugins-wp/simple-custom-post-order/
  *Description: Order Items (Posts, Pages, and Custom Post Types) using a Drag and Drop Sortable JavaScript.
- *Version: 2.4.1
+ *Version: 2.4.2
  *Author: Colorlib
  *Author URI: https://colorlib.com/wp/
  *Text Domain: simple-custom-post-order
@@ -46,10 +46,10 @@ class SCPO_Engine {
         add_action( 'admin_notices', array( $this, 'scporder_notice_not_checked' ) );
         add_action( 'wp_ajax_scporder_dismiss_notices', array( $this, 'dismiss_notices' ) );
 
-        add_action( 'plugins_loaded', array( $this, 'load_plugin_textdomain' ) );
+        add_action( 'plugins_loaded', array( $this, 'load_scpo_textdomain' ) );
     }
 
-    public function load_plugin_textdomain(){
+    public function load_scpo_textdomain(){
         load_plugin_textdomain( 'simple-custom-post-order', false, basename( dirname( __FILE__ ) ) . '/languages/' );
     }
 
